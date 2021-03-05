71a282e2914993416fd224f38945dfc4f1e58d56
Julian Barg <barg.julian@gmail.com>
2021-03-05 17:45:43 -0500

    Updated library and tried my hand on gtag again.

--- custom.js
+++ custom.js
@@ -1,1 +1,9 @@
-gtag('config', 'UA-188080844-1');
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3H0NSS66Y2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-3H0NSS66Y2');
+</script>
